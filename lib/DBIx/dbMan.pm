a56d686066474cb2c6e9de9661e01071eb928f63
Milan Sorm <sorm@milansorm.cz>
2014-01-17 07:53:23 +0100

    Preparation for 0.39 development

--- lib/DBIx/dbMan.pm
+++ lib/DBIx/dbMan.pm
@@ -1,7 +1,7 @@
 package DBIx::dbMan;
 
 =comment
-	dbMan 0.38
+	dbMan 0.39
 	(c) Copyright 1999-2014 by Milan Sorm, sorm@is4u.cz
 	All rights reserved.
 
@@ -19,7 +19,7 @@ use DBIx::dbMan::DBI;		# dbMan DBI interface package
 use DBIx::dbMan::MemPool;	# dbMan memory management system package
 use Data::Dumper;
 
-our $VERSION = '0.38';
+our $VERSION = '0.39';
 
 # constructor, arguments are hash of style -option => value, stored in internal attributes hash
 sub new {
